f4d21c1dfe38b84777eb2ce3ac15b9137a035241
Baturay Kayaturk <baturaykayaturk@hotmail.com>
2019-02-19 18:43:15 -0500

    Initial Commit

---
+++ functionsSignature/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  functionsSignature
+//
+//  Created by Baturay Kayatürk on 19.02.2019.
+//  Copyright © 2019 LambtonCollege. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
