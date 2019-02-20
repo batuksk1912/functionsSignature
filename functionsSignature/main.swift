058bcb2cfc18e1b34421652556bb23942b3257a0
Baturay Kayaturk <baturaykayaturk@hotmail.com>
2019-02-19 19:17:21 -0500

    functions signatures and closures.

--- functionsSignature/main.swift
+++ functionsSignature/main.swift
@@ -8,5 +8,44 @@
 
 import Foundation
 
-print("Hello, World!")
+let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+
+print(names)
+
+func backward(_ s1: String, _ s2: String) -> Bool {
+    return s1 < s2
+}
+
+func forward(_ s1: String, _ s2: String) -> Bool {
+    return s1 > s2
+}
+
+var reversedNames1 = names.sorted(by: backward)
+
+print(reversedNames1)
+
+var reversedNames2 = names.sorted(by: forward)
+
+print(reversedNames2)
+
+var reversedNames3 = names.sorted { (a: String, b: String) -> Bool in
+    return a > b
+}
+
+print(reversedNames3)
+
+var reversedNames4 = names.sorted { (a, b) -> Bool in
+    return a > b
+}
+
+print(reversedNames4)
+
+var reversedNames5 = names.sorted { $0 < $1 }
+
+print(reversedNames5)
+
+var reversedNamed6 = names.sorted(by: <)
+
+print(reversedNamed6)
+
 
